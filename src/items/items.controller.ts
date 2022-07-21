e23f0347ba36699e17757cb2474b8b6f07ef5373
Kakip1919 <invest9me@yahoo.co.jp>
2022-07-21 22:24:46 +0900

    nest js

---
+++ src/items/items.controller.ts
@@ -0,0 +1,47 @@
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {ItemsService} from "./items.service";
+import {Item} from "./item.model";
+import {ItemStatus} from "./item-status.enum";
+
+@Controller('items')
+export class ItemsController {
+    constructor(private readonly itemService: ItemsService) {
+    }
+
+    @Get()
+    findAll(): Item[] {
+        return this.itemService.findAll();
+    }
+
+    @Get(":id") // /items/id
+    findById(@Param("id") id: string): Item {
+        return this.itemService.findById(id)
+    }
+
+    @Post()
+    create(
+        @Body("id") id: string,
+        @Body("name") name: string,
+        @Body("price") price: number,
+        @Body("description") description: string
+    ): Item {
+        const item: Item = {
+            id,
+            name,
+            price,
+            description,
+            status: ItemStatus.ON_SALE
+        };
+        return this.itemService.create(item);
+    }
+
+    @Patch(":id")
+    updateStatus(@Param("id") id: string): Item {
+        return this.itemService.updateStatus(id);
+    }
+
+    @Delete(":id")
+    delete(@Param("id") id: string): void{
+        this.itemService.delete(id);
+    }
+}
